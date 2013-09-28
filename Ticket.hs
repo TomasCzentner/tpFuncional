d6b9332543c7fdbab627cab6457aaea6788c2431
promachov@dc.uba.ar <promachov@dc.uba.ar>
2013-09-28 18:50:19 -0300

    peliculaMenosVista

--- Ticket.hs
+++ Ticket.hs
@@ -9,5 +9,36 @@ nuevoT::Pelicula->Sala->Bool->Ticket
 nuevoT p s b | b == False 	= TicketSinUsar s p
 			 | otherwise 	= TicketUsado (nuevoT p s (not b))
 
---peliculaT::Ticket->
+peliculaT::Ticket->Pelicula
+peliculaT (TicketSinUsar s p) = p
+peliculaT (TicketUsado (TicketSinUsar s p)) = p
 
+salaT::Ticket->Sala
+salaT (TicketSinUsar s p) = s
+salaT (TicketUsado (TicketSinUsar s p)) = s
+
+usadoT::Ticket->Bool
+usadoT (TicketSinUsar s p) = False
+usadoT (TicketUsado (TicketSinUsar s p)) = True
+
+usarT::Ticket->Ticket
+usarT (TicketSinUsar s p) = (TicketUsado (TicketSinUsar s p))
+usarT (TicketUsado t1) 	  = t1
+
+{-peliculaMenosVistaT::[Ticket]->Pelicula
+peliculaMenosVista [(TicketSinUsar s p)] = p 
+peliculaMenosVistaT ((TicketSinUsar s p):ts) = 
+peliculaMenosVistaT ((TicketUsado(TicketSinUsar(s p)))):ts) =
+
+apariciones::Char->[Char]->Int
+apariciones a (b:as) | a == b = 1 + apariciones a as
+					 | otherwise = apariciones a as
+apariciones a [] = 0
+
+aparicionesPelis::Pelicula->[Ticket]->Int
+aparicionesPelis p ()-}
+
+s1 = ['a','b','c','a']
+
+t1 = nuevoT p1 5 True
+t2 = nuevoT p1 6 False
