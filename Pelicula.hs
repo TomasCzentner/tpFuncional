f0d13a89e9b0ef53b84105c5731a8063660c2cf2
Andreas <remruts@gmail.com>
2013-09-28 15:53:55 -0300

    Con generos, actores, 3d

--- Pelicula.hs
+++ Pelicula.hs
@@ -5,16 +5,16 @@ import Tipos
 data Pelicula = P Nombre [Genero] [Actor] Bool deriving (Show, Eq)
 
 nuevaP:: Nombre -> [Genero] -> [Actor] -> Bool -> Pelicula
-nuevaP n g a b = (P n (auxGenerosSinRepetidos g) (auxActoresSinRepetidos a) b)
+nuevaP n gs as b = (P n (auxGenerosSinRepetidos g) (auxActoresSinRepetidos a) b)
 
 nombreP:: Pelicula -> Nombre
 nombreP (P n _ _ _) = n
 
 generosP:: Pelicula -> [Genero]
-generosP (P _ g _ _) = g
+generosP (P _ gs _ _) = gs
 
 actoresP:: Pelicula -> [Actor]
-actoresP (P _ _ a _) = a
+actoresP (P _ _ as _) = as
 
 es3DP:: Pelicula -> Bool
 es3DP (P _ _ _ b) = b
