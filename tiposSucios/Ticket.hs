af51a9e5e2102b10c475f431db3e4df033078d17
Andreas <remruts@gmail.com>
2013-09-28 15:46:51 -0300

    Sin tiposSucios

--- tiposSucios/Ticket.hs
+++
@@ -1,6 +0,0 @@
-module Ticket (Ticket, nuevoT, salaT, peliculaT, usadoT, usarT, peliculaMenosVistaT, todosLosTicketsParaLaMismaSalaT, cambiarSalaT) 	where
-
-import Tipos
-import Pelicula
-
-data Ticket = TicketSinUsar Sala Pelicula | TicketUsado Ticket deriving (Show, Eq)
--- tiposSucios/Tipos.hs
+++
@@ -1,7 +0,0 @@
-module Tipos where
-
-type Sala = Int
-type Nombre = String
-type Actor = String
-
-data Genero = Aventura | Comedia | Drama | Romantica | Terror deriving (Show, Eq)
